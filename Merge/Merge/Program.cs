1eee171b7ece2b3b1fa063156e46b087f18202cc
지훈 김 <ji_hun23@naver.com>
2020-10-10 23:44:17 +0900

    First

---
+++ Merge/Merge/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merge
+{
+    class Program
+    {
+        static int calculate(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a와 b의 계산은 :{0}입니다.", calculate(5, 5));
+        }
+    }
+}
